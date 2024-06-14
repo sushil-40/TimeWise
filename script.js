f9e003e937253ad7b604c18a6b2d598b45f2422b
Sushil <grayson.9999@gmail.com>
2024-06-14 21:00:25 +1000

    Adding data from form to entry table

--- script.js
+++ script.js
@@ -16,5 +16,35 @@ const handleOnSubmit = (e) => {
   };
 
   taskList.push(obj);
+  //   console.log(taskList);
+
+  displayEntryList();
+};
+
+//Display items in tables
+
+const displayEntryList = () => {
   console.log(taskList);
+  //   console.log("First");
+  let str = "";
+
+  const entryElm = document.getElementById("entryList");
+
+  taskList.map((item, i) => {
+    str += `<tr>
+                  <td>${i + 1}</td>
+                  <td>${item.task}</td>
+                  <td>${item.hr}</td>
+                  <td class="text-end">
+                    <button class="btn btn-danger">
+                      <i class="fa-solid fa-trash"></i>
+                    </button>
+                    <button class="btn btn-success">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                  </td>
+                </tr>`;
+  });
+
+  entryElm.innerHTML = str;
 };
